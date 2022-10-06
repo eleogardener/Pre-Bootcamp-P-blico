7203ab70ab44c2bae4ea3a2ec767ccd6b5c7e6b1
eleo <73608810+eleogardener@users.noreply.github.com>
2022-10-06 15:20:33 -0300

    Add files via upload

---
+++ Eleo_Vergara_JS_Jugueteo.js
@@ -0,0 +1,10 @@
+ var alturaNiño = 
+ muestraSiElNiñoPuedeSubirALaMontañaRusa();
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(){
+    if (alturaNiño>=52){
+        console.log("¡Súbete, chico!");
+    }
+    else{
+        console.log ("Lo siento, chico. Tal vez el próximo año");
+    }
+}
